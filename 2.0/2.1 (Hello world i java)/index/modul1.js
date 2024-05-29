1c9dd19f1c35c6ad24163885ef71213865ce8721
LeonardWiidh <leonard.wiidh@elev.ga.ntig.se>
2024-05-29 09:44:08 +0200

    first commit

---
+++ 2.0/2.1 (Hello world i java)/index/modul1.js
@@ -0,0 +1,10 @@
+console.log("tjeeeena")
+const btn = document.getElementById("button")
+const btnHello = document.getElementById("abooo")
+function myFunction() {
+    alert("hej detta detta är en larmbox");
+    }
+function myFunctionn() {
+    btnHello.innerHTML = "hesan svensan";
+    btn.classList.toggle("newbackground")
+}
